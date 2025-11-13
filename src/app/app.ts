c3795bdee0778f1e465acaa464e073601031b852
EdogawaCoder <edogawa.araujo@gmail.com>
2025-11-13 19:20:33 -0300

    configuração do bootstrap e integração com o serviço de consulta de produtos,

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,4 +1,5 @@
-import { Component, signal } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject,} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
@@ -8,5 +9,20 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('ProdutosWeb');
+  //instaciando a biblioteca de HTTP CLient
+  
+  private http=inject(HttpClient);
+
+  //função para realizar a pesquisa de produtos
+  consultarProduto() {
+
+    console.log("consulta de produtos iniciada");
+
+ //realizando a requisição HTTP para obter a lista de produtos
+ this.http.get('http://localhost:5114/api/produtos')
+  .subscribe((produtos)=>{//capturando a resposta da requisição
+    //imprimindo os produtos no console do navegador
+    console.log(produtos);
+   });
+}
 }
